100b3c4d9dd42353252a38a607388119468bb7d3
ErkinbekovBilim <erkinbekovbilim298@gmail.com>
2023-04-01 23:28:54 +0600

    Убрал кнопку

--- app/src/main/java/com/example/erkinbekovbilimdz_all_4/MainActivity.kt
+++ app/src/main/java/com/example/erkinbekovbilimdz_all_4/MainActivity.kt
@@ -32,7 +32,8 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home,
                 R.id.navigation_dashboard,
                 R.id.navigation_notifications,
-                R.id.taskFragment
+                R.id.taskFragment,
+                R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
