ac1334f197be110cf497d04509b69ae4a84c1666
ErkinbekovBilim <erkinbekovbilim298@gmail.com>
2023-04-01 23:06:27 +0600

    Initial commit

---
+++ app/src/main/java/com/example/erkinbekovbilimdz_all_4/ui/home/HomeFragment.kt
@@ -0,0 +1,59 @@
+package com.example.erkinbekovbilimdz_all_4.ui.home
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.erkinbekovbilimdz_all_4.R
+import com.example.erkinbekovbilimdz_all_4.databinding.FragmentHomeBinding
+import com.example.erkinbekovbilimdz_all_4.model.Task
+import com.example.erkinbekovbilimdz_all_4.ui.home.adapter.TaskAdapter
+import com.example.erkinbekovbilimdz_all_4.ui.task.TaskFragment.Companion.TASK_KEY
+import com.example.erkinbekovbilimdz_all_4.ui.task.TaskFragment.Companion.TASK_REQUEST
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    private val adapter = TaskAdapter()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setFragmentResultListener(TASK_REQUEST){key, bundle ->
+            val result = bundle.getSerializable(TASK_KEY) as Task
+            adapter.addTask(result)
+            
+        }
+        binding.recyclerView.adapter = adapter
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.taskFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
